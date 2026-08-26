19fe57e9b3cc1c56cfd686025c29ac290229c7c4
guilhermexmada <guilhermeshimadapereira@gmail.com>
2026-08-26 20:45:02 -0300

    remake: deleta API JavaScript

--- src/queues/imageQueue.js
+++
@@ -1,8 +0,0 @@
-import { Queue } from 'bullmq'
-import { redisConfig } from '../config/redis-config.js'
-
-const imageQueue = new Queue('analysis-queue', {
-    connection: redisConfig
-})
-
-export default imageQueue
